e3e1222e542bf34d74cf7d25191cddfc0ed68079
pupaandlupa2213 <andrey_sgk@mail.ru>
2023-02-01 02:03:48 +0300

    Initial commit

---
+++ src/main/java/Pages/IncomingMessagesPage.java
@@ -0,0 +1,27 @@
+package Pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+import java.util.List;
+
+public class IncomingMessagesPage extends Base{
+    public IncomingMessagesPage(WebDriver driver) {
+        super(driver);
+    }
+    private final By listMessagesLocator = By.xpath("//div[@class='llc__content']");
+    private final By listLeftPanelLocator = By.xpath("//div[@class='nav__folder-name nav__folder-name_shared']");
+
+    public IncomingMessagesPage moveMessage(int numberMessage, int cellNumberLeftPanel){
+        List<WebElement> list = driver.findElements(listMessagesLocator);
+        WebElement first = list.get(numberMessage);
+        List<WebElement> list2 = driver.findElements(listLeftPanelLocator);
+        WebElement second = list2.get(cellNumberLeftPanel);
+        new Actions(driver)
+                .dragAndDrop(first,second)
+                .perform();
+        return this;
+    }
+}
